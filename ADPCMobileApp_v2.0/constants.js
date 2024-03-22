3407011387d1f35868f37e71da296ffa8b80e225
felixkoczan <130245831+felixkoczan@users.noreply.github.com>
2024-03-22 13:14:51 +0100

    deletion of unncecessary files

--- ADPCMobileApp_v2.0/constants.js
+++ ADPCMobileApp_v2.0/constants.js
@@ -1,3 +1,3 @@
-export const openAIKey = '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';
+export const openAIKey = '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';
 export const SERVICE_UUID = '4fafc201-1fb5-459e-8fcc-c5c9c331914b';
 export const CHARACTERISTIC_UUID = 'beb5483e-36e1-4688-b7f5-ea07361b26a8';
