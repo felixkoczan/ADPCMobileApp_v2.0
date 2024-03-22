240f55e69f0769d17d454cc40130edb7b9dbdec5
felixkoczan <130245831+felixkoczan@users.noreply.github.com>
2024-03-22 20:37:13 +0100

    reworked all screens and BLE functionality

--- ADPCMobileApp_v2.0/App.js
+++ ADPCMobileApp_v2.0/App.js
@@ -1,15 +1,32 @@
 // Import necessary hooks and modules from React, React Native, and other dependencies.
-import React, { useEffect } from 'react';
-import { PermissionsAndroid, Platform } from 'react-native';
+import React, { useEffect, useContext, useState } from 'react';
+import { PermissionsAndroid, Platform, StatusBar, StyleSheet, ActivityIndicator, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'; // For managing navigation.
 import AppNavigation from './AppNavigation'; // Custom navigation setup.
 import ThemeProvider from './ThemeProvider'; // Provides theming capabilities.
-import {BLEConnect, BLEProvider} from './BLEContext'; // Context for BLE operations.
+import { BLEProvider} from './BLEContext';
+import ThemeContext from './ThemeContext';
+
+
+const ThemeAwareStatusBar = () => {
+  const { theme } = useContext(ThemeContext);
+
+  useEffect(() => {
+    StatusBar.setBarStyle(theme.dark ? 'light-content' : 'dark-content', true);
+    StatusBar.setBackgroundColor(theme.backgroundColor);
+  }, [theme]);
+
+  return null;
+};
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  const { theme } = useContext(ThemeContext);
+  
 
   // Use the useEffect hook to request necessary permissions upon app initialization.
   useEffect(() => {
+
     const requestBluetoothPermissions = async () => {
       try {
         // Check the platform and version to request appropriate permissions.
@@ -22,6 +39,7 @@ const App = () => {
             PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
           ]);
           console.log('Permissions status:', permissionsStatus);
+          permissionsGranted = Object.values(permissionsStatus).every(status => status === PermissionsAndroid.RESULTS.GRANTED);
         } else if (Platform.OS === 'android' && Platform.Version >= 23) {
           // For Android 6 (Marshmallow, API level 23) to Android 11, request location permission.
           // This is necessary for Bluetooth scanning due to the need to access location information.
@@ -39,23 +57,43 @@ const App = () => {
         // Catch and log any errors that occur during the permission request process.
         console.warn(err);
       }
+      setLoading(!permissionsGranted);
     };
 
-    // Call the function to request Bluetooth permissions.
     requestBluetoothPermissions();
-  }, []); // The empty dependency array ensures this effect runs only once when the component mounts.
+  }, []);
 
-  // Render the app's components wrapped in context providers for BLE and theming.
-  // The NavigationContainer wraps the app's navigation setup.
+  if (loading) {
+    return (
+      <View style={[styles.container, { backgroundColor: theme?.backgroundColor || '#FFF' }]}>
+        
+        <ActivityIndicator size="large" color="#0000ff" />
+        <Text style={styles.loadingText}>Requesting permissions...</Text>
+      </View>
+    );
+  }
+
   return (
     <BLEProvider>
       <ThemeProvider>
-        <NavigationContainer>
-          <AppNavigation />
+        <ThemeAwareStatusBar />
+          <NavigationContainer>
+            <AppNavigation />
         </NavigationContainer>
       </ThemeProvider>
     </BLEProvider>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  loadingText: {
+    marginTop: 20,
+  },
+});
 
 export default App;
--- ADPCMobileApp_v2.0/screens/HomeScreen.js
+++ ADPCMobileApp_v2.0/screens/HomeScreen.js
@@ -4,7 +4,7 @@ import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
 // Import the ThemeContext to use for dynamic theming throughout the app.
 import ThemeContext from '../ThemeContext';
 // Import an image to be used as a logo within the component.
-import Logo from '../assets/ADPC_Logo_high.png';
+import Logo from '../assets/adpc_logo_high.png';
 // Import a custom hook from BLEContext to manage BLE operations.
 import { useBLE } from '../BLEContext';
 
@@ -14,30 +14,28 @@ const HomeScreen = ({ navigation }) => {
     const { theme } = useContext(ThemeContext);
     // Access BLE-related functionalities (starting a scan and checking if a scan is ongoing)
     // through the custom hook useBLE.
-    const { startScan, isScanning } = useBLE();
+    const { startScan } = useBLE();
     
     
     return (
         <View style={[styles.container, { backgroundColor: theme.backgroundColor }]}>
             <Image source={Logo} style={styles.logo} />
             <Text style={[styles.title, { color: theme.textColor }]}>Welcome to ADPC-IoT</Text>
-          <View style={styles.buttonContainer}>
             <TouchableOpacity
               style={[styles.discoverButton, 
-              { backgroundColor: theme.buttonTextColor }]}
+              { backgroundColor: theme.backgroundColor, borderColor: theme.textColor }]}
               onPress={() => {
                 // Call the startScan function
                 startScan();
                 // Navigate to the 'Device Registration' screen
                 navigation.navigate('Discover Devices');
               }}>
-              <Text style={[styles.buttonText, { color: theme.backgroundColor }]}>
+              <Text style={[styles.buttonText, { color: theme.textColor }]}>
                 Scan for Devices
               </Text>
             </TouchableOpacity>
-            </View>
             <TouchableOpacity
-                style={[styles.button, { backgroundColor: theme.buttonColor }]}
+                style={[styles.button, { backgroundColor: theme.buttonColor, borderColor: theme.textColor }]}
                 onPress={() => navigation.navigate('Manage Consents')}>
                 <Text style={[styles.buttonText, { color: theme.buttonTextColor }]}>Manage Consents</Text>
             </TouchableOpacity>
@@ -69,22 +67,24 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     color: '#333',
 },
-
   button: {
-    padding: 15,
-    borderRadius: 5,
-    alignItems: 'center', 
-    justifyContent: 'center', 
-    marginBottom: 10,
+    marginTop: 10,
+    padding: 10,
+    alignItems: 'center',
+    borderWidth: 1, 
+    borderRadius: 20, 
+    paddingHorizontal: 15, 
+    fontSize: 16, 
     width: '60%',
   },
   discoverButton: {
-    padding: 15,
-    borderRadius: 5,
-    alignItems: 'center', 
-    justifyContent: 'center',
-    marginTop: 20,
-    marginBottom: 20,
+    marginTop: 10,
+    padding: 10,
+    alignItems: 'center',
+    borderWidth: 1, 
+    borderRadius: 20, 
+    paddingHorizontal: 15, 
+    fontSize: 16, 
     width: '60%',
   },
   buttonText: {
@@ -96,9 +96,6 @@ const styles = StyleSheet.create({
     width: 223,
     height: 100,
     marginBottom: 20, 
-  },
-  buttonContainer: {
-    alignItems: 'stretch'
   },
 });
 
